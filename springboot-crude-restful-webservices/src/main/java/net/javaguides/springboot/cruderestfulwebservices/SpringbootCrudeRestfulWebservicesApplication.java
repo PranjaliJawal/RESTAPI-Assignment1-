cd1c630e1e90dde05d390be97dba8141e0527555
PranjaliJawal <pranjalijawal079@gmail.com>
2021-02-02 13:09:37 +0530

    assignment 1

---
+++ springboot-crude-restful-webservices/src/main/java/net/javaguides/springboot/cruderestfulwebservices/SpringbootCrudeRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.cruderestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudeRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudeRestfulWebservicesApplication.class, args);
+	}
+
+}
